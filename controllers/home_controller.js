5aa176ff33bbdf4b141b9a90d148c9fe4c623916
Ankit Singh <monusingh928469@gmail.com>
2023-08-07 20:56:20 +0530

    Initial commit

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,20 @@
-module.exports.home = function(req, res){
-    
-    return res.render('home', {
-        title: "Home"
+const Post = require("../models/post");
+
+module.exports.home = function (req, res) {
+  Post.find({})
+    .populate("user")
+    .then((post) => {
+      return res.render("home", {
+        title: "Codial | Home",
+        posts: post,
+      });
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
     });
-}
+};
 
 // module.exports.actionName = function(req, res){}
