1bdd5a22d3346d039eca28d748b5fec762b47fb2
Ankit Singh <monusingh928469@gmail.com>
2023-08-07 19:47:14 +0530

    Initial commit

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,6 +8,9 @@ module.exports.profile = function (req, res) {
 
 // render the signUp page
 module.exports.signUp = function (req, res) {
+  if(req.isAuthenticated()){
+    return res.redirect('/users/profile');
+  }
   return res.render("user_sign_up", {
     title: "User SignUp",
   });
@@ -15,6 +18,9 @@ module.exports.signUp = function (req, res) {
 
 //render the signIn page
 module.exports.signIn = function (req, res) {
+  if(req.isAuthenticated()){
+   return res.redirect('/users/profile');
+  }
   return res.render("user_sign_in", {
     title: "User SignIn",
   });
@@ -25,26 +31,34 @@ module.exports.create = function (req, res) {
   if (req.body.password != req.body.confirm_password) {
     return res.redirect("back");
   }
-
-  User.findOne({ email: req.body.email }, function (err, user) {
-    if (err) {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        User.create(req.body, function (err, user) {
+          if (err) {
+            console.log("Error in creating user!");
+            return;
+          }
+          return res.redirect("/users/sign-in");
+        });
+      } else {
+        return res.redirect("back");
+      }
+    })
+    .catch((err) => {
       console.log("Error in finding user in signing up");
       return;
-    }
-
-    if (!user) {
-      User.create(req.body, function (err, user) {
-        if (err) {
-          console.log("Error in creating user!");
-          return;
-        }
-        return res.redirect("/users/sign-in");
-      });
-    } else {
-      return res.redirect("back");
-    }
-  });
+    });
 };
 
 // SignIn and create a session ffor the user
-module.exports.createSession = function (req, res) {};
+module.exports.createSession = function (req, res) {
+  return res.redirect("/");
+};
+
+module.exports.destroySession = function(req,res){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+}
--- index.js
+++ index.js
@@ -1,33 +1,75 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
-const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
+//used for session cookies
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+
+const MongoStore = require('connect-mongo');
+
+const sassMiddleware = require('node-sass-middleware');
+
+
+app.use(sassMiddleware({
+  src:'./assets/scss',
+  dest:'./assets/css',
+  debug: true,
+  outputStyle: 'extended',
+  prefix: '/css'
+}));
 app.use(express.urlencoded());
+
 app.use(cookieParser());
 
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 
-// use express router
-app.use('/', require('./routes'));
 
 // set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// mongo store is used to store the sesion cookie in the database
+app.use(session({
+    name:'codial',
+    //Todo change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: MongoStore.create({
+      mongoUrl: 'mongodb://127.0.0.1:27017/test',
+      autoRemove: 'disabled'
+    }, function(err){
+      console.log(err || 'connect-mongodb setup ok');
+    })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.checkAuthenticatedUser);
+
+// use express router
+app.use("/", require("./routes"));
 
 
-app.listen(port, function(err){
-    if (err){
-        console.log(`Error in running the server: ${err}`);
-    }
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
 
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
