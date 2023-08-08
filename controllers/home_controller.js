8b8c8bb9f5c7607c716bef6589c60e9991c6580e
Ankit Singh <monusingh928469@gmail.com>
2023-08-08 10:54:38 +0530

    Initial commit

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,6 +3,12 @@ const Post = require("../models/post");
 module.exports.home = function (req, res) {
   Post.find({})
     .populate("user")
+    .populate({
+      path: 'comments',
+      populate: {
+        path: 'user'
+      }
+    })
     .then((post) => {
       return res.render("home", {
         title: "Codial | Home",
