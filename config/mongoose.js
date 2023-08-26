81d1fff4a9f5a81dbada73f1c313b9cad3a26faa
Ankit Kumar Singh <monusingh928469@gmail.com>
2023-08-26 20:35:46 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://monusingh928469:2Gfp8ory6X6uik0R@cluster0.j7aypsx.mongodb.net/?retryWrites=true&w=majority');
+  await mongoose.connect('mongodb://monusingh928469:mWiV22lTJCs2HXFD@ac-xu9jsst-shard-00-00.j7aypsx.mongodb.net:27017,ac-xu9jsst-shard-00-01.j7aypsx.mongodb.net:27017,ac-xu9jsst-shard-00-02.j7aypsx.mongodb.net:27017/?ssl=true&replicaSet=atlas-x3v0e5-shard-0&authSource=admin&retryWrites=true&w=majority');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
