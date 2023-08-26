b567687b136cd1efb55def9a4e0664f6c5a9a5cb
Ankit Kumar Singh <monusingh928469@gmail.com>
2023-08-26 20:55:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(
     },
     store: MongoStore.create(
       {
-        mongoUrl: 'mongodb://monusingh928469:mWiV22lTJCs2HXFD@ac-xu9jsst-shard-00-00.j7aypsx.mongodb.net:27017,ac-xu9jsst-shard-00-01.j7aypsx.mongodb.net:27017,ac-xu9jsst-shard-00-02.j7aypsx.mongodb.net:27017/?ssl=true&replicaSet=atlas-x3v0e5-shard-0&authSource=admin&retryWrites=true&w=majority',
+        mongoUrl: 'mongodb://user20000:Pass1234@ac-xu9jsst-shard-00-00.j7aypsx.mongodb.net:27017,ac-xu9jsst-shard-00-01.j7aypsx.mongodb.net:27017,ac-xu9jsst-shard-00-02.j7aypsx.mongodb.net:27017/?ssl=true&replicaSet=atlas-x3v0e5-shard-0&authSource=admin&retryWrites=true&w=majority',
         autoRemove: "disabled",
       },
       function (err) {
